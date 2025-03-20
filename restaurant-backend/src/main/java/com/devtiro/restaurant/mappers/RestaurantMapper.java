5dfcf49f02c52cd3f8be3658e3de7c07d3ecc722
Andriy <karpus.andrii@proton.me>
2025-03-20 20:04:57 +0100

    Restaurant Search DTO and Mapper

--- restaurant-backend/src/main/java/com/devtiro/restaurant/mappers/RestaurantMapper.java
+++ restaurant-backend/src/main/java/com/devtiro/restaurant/mappers/RestaurantMapper.java
@@ -4,11 +4,16 @@ import com.devtiro.restaurant.domain.RestaurantCreateUpdateRequest;
 import com.devtiro.restaurant.domain.dtos.GeoPointDto;
 import com.devtiro.restaurant.domain.dtos.RestaurantCreateUpdateRequestDto;
 import com.devtiro.restaurant.domain.dtos.RestaurantDto;
+import com.devtiro.restaurant.domain.dtos.RestaurantSummaryDto;
 import com.devtiro.restaurant.domain.entities.Restaurant;
+import com.devtiro.restaurant.domain.entities.Review;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
@@ -17,7 +22,15 @@ public interface RestaurantMapper {
 
     RestaurantDto toRestaurantDto(Restaurant restaurant);
 
+    @Mapping(source = "reviews", target = "totalReviews", qualifiedByName = "populateTotalReviews")
+    RestaurantSummaryDto toRestaurantSummaryDto(Restaurant restaurant);
+
     @Mapping(target = "latitude", expression = "java(geoPoint.getLat())")
     @Mapping(target = "longitude", expression = "java(geoPoint.getLon())")
     GeoPointDto toGeoPointDto(GeoPoint geoPoint);
+
+    @Named("populateTotalReviews")
+    private Integer populateTotalReviews(List<Review> reviews) {
+        return reviews.size();
+    }
 }
