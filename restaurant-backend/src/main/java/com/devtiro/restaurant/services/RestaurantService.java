07679ea3afed3bac7de9d7bbc077af0bf5c1a0e1
Andriy <karpus.andrii@proton.me>
2025-03-20 19:52:14 +0100

    Restaurant Search Service

--- restaurant-backend/src/main/java/com/devtiro/restaurant/services/RestaurantService.java
+++ restaurant-backend/src/main/java/com/devtiro/restaurant/services/RestaurantService.java
@@ -2,8 +2,19 @@ package com.devtiro.restaurant.services;
 
 import com.devtiro.restaurant.domain.RestaurantCreateUpdateRequest;
 import com.devtiro.restaurant.domain.entities.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface RestaurantService {
 
     Restaurant createRestaurant(RestaurantCreateUpdateRequest request);
+
+    Page<Restaurant> searchRestaurants(
+            String query,
+            Float minRating,
+            Float latitude,
+            Float longitude,
+            Float radius,
+            Pageable pageable
+    );
 }
